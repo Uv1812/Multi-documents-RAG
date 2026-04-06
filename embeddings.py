dbf3ab6a97b216a0e3c16cd1e0f3a848ab1553c7
Urvisha Gothadiya <148183929+Uv1812@users.noreply.github.com>
2026-04-06 09:51:58 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,8 +1,12 @@
 import os
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from config import hugging_token
 
-EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+os.environ["HF_TOKEN"] = hugging_token
 
-print("Loading embedding model...")
-embedder = HuggingFaceEmbeddings(model_name=EMBED_MODEL)
-print("Embedding model ready.")
+embedder = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=hugging_token
+)
+
+print("Embedder configured — using HuggingFace Inference API.")
